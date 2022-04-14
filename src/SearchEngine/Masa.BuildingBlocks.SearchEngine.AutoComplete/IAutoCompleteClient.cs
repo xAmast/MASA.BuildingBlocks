b2a34f3ed5adc4155e901cf021cde322f0cac959
zhenlei520 <wangzhenlei520@gmail.com>
2022-04-14 16:56:57 +0800

    feat(AutoComplete): add DeleteAsync and DeleteMultiAsyncAsync methods

--- src/SearchEngine/Masa.BuildingBlocks.SearchEngine.AutoComplete/IAutoCompleteClient.cs
+++ src/SearchEngine/Masa.BuildingBlocks.SearchEngine.AutoComplete/IAutoCompleteClient.cs
@@ -13,12 +13,22 @@ public interface IAutoCompleteClient
         where TAudoCompleteDocument : AutoCompleteDocument<TValue>;
 
     Task<SetResponse> SetAsync<TValue>(
-        AutoCompleteDocument<TValue>[] results,
+        IEnumerable<AutoCompleteDocument<TValue>> results,
         SetOptions? options = null,
         CancellationToken cancellationToken = default);
 
     Task<SetResponse> SetAsync<TAudoCompleteDocument, TValue>(
-        TAudoCompleteDocument[] documents,
+        IEnumerable<TAudoCompleteDocument> documents,
         SetOptions? options = null,
-        CancellationToken cancellationToken = default) where TAudoCompleteDocument : AutoCompleteDocument<TValue> ;
+        CancellationToken cancellationToken = default) where TAudoCompleteDocument : AutoCompleteDocument<TValue>;
+
+    Task<DeleteResponse> DeleteAsync<TValue>(AutoCompleteDocument<TValue> document);
+
+    Task<DeleteMultiResponse> DeleteMultiAsyncAsync<TValue>(IEnumerable<AutoCompleteDocument<TValue>> documents);
+
+    Task<DeleteResponse> DeleteAsync<TAudoCompleteDocument, TValue>(TAudoCompleteDocument document)
+        where TAudoCompleteDocument : AutoCompleteDocument<TValue>;
+
+    Task<DeleteMultiResponse> DeleteMultiAsyncAsync<TAudoCompleteDocument, TValue>(IEnumerable<TAudoCompleteDocument> documents)
+        where TAudoCompleteDocument : AutoCompleteDocument<TValue>;
 }
---
+++ src/SearchEngine/Masa.BuildingBlocks.SearchEngine.AutoComplete/Response/DeleteMultiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Masa.BuildingBlocks.SearchEngine.AutoComplete.Response;
+public class DeleteMultiResponse : ResponseBase
+{
+    public List<DeleteRangeResponseItems> Data { get; set; }
+
+    public DeleteMultiResponse(bool isValid, string message) : base(isValid, message)
+    {
+
+    }
+}
